510bc75f750decaaa02355fc8f591a526db29953
Kirito9957 <149939293+Kirito9957@users.noreply.github.com>
2023-12-20 09:45:02 +0530

    Update config.js

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,4 @@
-const prefix = ['!']; // You can use multiple prefix eg. !, ., -, ?
-const owner_id = ['']; // Put your number with code country eg. 81xxxxxxxx
+const prefix = ['#']; // You can use multiple prefix eg. !, ., -, ?
+const owner_id = ['919957594778']; // Put your number with code country eg. 81xxxxxxxx
 
 export { prefix, owner_id };
